74bfabc8c147d761c9cd4212373e77a97c21d239
admin_Object <13653054704@163.com>
2020-05-07 09:35:05 +0800

    init

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+* @Description: 用户Controller
+* @Author: admin_Object
+* @Date:  2020/5/6 19:57
+*/
+
+@RestController
+@RequestMapping("/testBoot")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("getUser/{id}")
+    public String GetUser(@PathVariable int id){
+        return userService.getUserById(id).toString();
+    }
+}
